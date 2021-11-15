fdeacdbe219dace42ce628e6c0753e53613d5769
Alex R <alexander.m.rankin@gmail.com>
2021-11-15 01:00:22 -0500

    Begin page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './index/header/header.component';
+import { NavComponent } from './common/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
